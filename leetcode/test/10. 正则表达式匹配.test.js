ea559ce2b24ff4311645cd6eeb2cb3836a639852
wangxianming <1842344641@qq.com>
2019-12-08 17:13:42 +0800

    题目456 85

---
+++ leetcode/test/10. 正则表达式匹配.test.js
@@ -0,0 +1,5 @@
+import regModeStr from '../code/10. 正则表达式匹配'
+
+test('10. 正则表达式匹配', () => {
+    expect(regModeStr('aa', 'a')).toBe(false)
+})
---
+++ leetcode/test/682. 棒球比赛.test.js
@@ -0,0 +1,5 @@
+import baseball from '../../leetcode/code/682. 棒球比赛'
+
+test('682. 棒球比赛', () => {
+    expect(baseball(["5", "2", "C", "D", "+"])).toBe(30)
+});
